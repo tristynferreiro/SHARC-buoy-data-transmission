dbeab6a8638f1ebe65a07fdc4ecc3de31a8ed331
ShameeraC <s.cassim105@gmail.com>
2022-09-09 13:29:48 +0200

    updated fft script

--- Software/Testing/FFTScript.m
+++ Software/Testing/FFTScript.m
@@ -16,6 +16,11 @@ tmp6 = X(:,8);
 Gyro = [tmp4, tmp5, tmp6];
 GyroCorr = [(tmp4-mean(tmp4)), (tmp5 - mean(tmp5)), (tmp6-mean(tmp6))];
 
+Temp = X(:,9);
+TempCorr = Temp - mean(Temp);
+
+AllCorr = X - mean(X);
+
 
 Fs = 1000000;                                       % Sampling frequency
 T = 1/Fs;                                           % Sampling period
@@ -25,6 +30,8 @@ t = Time;                                   % Time vector
 Fn = Fs/2;                                          % Nyquist Frequency
 FAcc = fft(Acc)/L;                                      % Fourier Transform
 FGyro = fft(Gyro)/L;
+FTemp = fft(Temp)/L;
+FAll = fft(X)/L;
 Fv = linspace(0, 1, fix(L/2)+1)*Fn;                 % Frequency Vector
 Iv = 1:length(Fv);                                  % Index Vector
 
@@ -44,10 +51,28 @@ xlabel('Frequency (Hz)')
 ylabel('Amplitude')
 saveas(2, "gyroFFT.png");
 
+figure(3)
+plot(Fv, abs(FTemp(Iv))*2)
+grid
+title('Fourier Transform Of Temperature Data')
+xlabel('Frequency (Hz)')
+ylabel('Amplitude')
+saveas(2, "tempFFT.png");
+
+figure(4)
+plot(Fv, abs(FAll(Iv))*2)
+grid
+title('Fourier Transform Of All Data')
+xlabel('Frequency (Hz)')
+ylabel('Amplitude')
+saveas(2, "combinedFFT.png");
+
 FAccdcoc = fft(AccCorr)/L;                          % Fourier Transform (D-C Offset Corrected)
 FGyrodcoc = fft(GyroCorr)/L;                          % Fourier Transform (D-C Offset Corrected)
+FTempdcoc = fft(TempCorr)/L;                          % Fourier Transform (D-C Offset Corrected)
+FAlldcoc = fft(AllCorr)/L;                          % Fourier Transform (D-C Offset Corrected)
 
-figure(3)
+figure(5)
 plot(Fv, abs(FAccdcoc(Iv))*2)
 grid
 title('Fourier Transform Of D-C Offset Corrected Accelerometer Data')
@@ -56,12 +81,28 @@ ylabel('Amplitude')
 saveas(2, "accCorrFFT.png");
 
 
-figure(4)
+figure(6)
 plot(Fv, abs(FGyrodcoc(Iv))*2)
 grid
 title('Fourier Transform Of D-C Offset Corrected Gyroscope Data')
 xlabel('Frequency (Hz)')
 ylabel('Amplitude')
 saveas(4, "GyroCorrFFT.png");
+
+figure(7)
+plot(Fv, abs(FTempdcoc(Iv))*2)
+grid
+title('Fourier Transform Of D-C Offset Corrected Temperature Data')
+xlabel('Frequency (Hz)')
+ylabel('Amplitude')
+saveas(4, "TempCorrFFT.png");
+
+figure(8)
+plot(Fv, abs(FAlldcoc(Iv))*2)
+grid
+title('Fourier Transform Of D-C Offset Corrected All Data')
+xlabel('Frequency (Hz)')
+ylabel('Amplitude')
+saveas(4, "CombinedCorrFFT.png");
 
 
