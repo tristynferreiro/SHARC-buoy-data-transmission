6fac552b818f8d4403275b188078466ee55bc435
ShameeraC <s.cassim105@gmail.com>
2022-09-12 20:04:38 +0200

    fputc replaced wity fprintf in encryption subsystem

--- Software/encryption/RSA/rsa.c
+++ Software/encryption/RSA/rsa.c
@@ -164,9 +164,8 @@ void encrypt2(char* msg[]) {
         for (i = 0; msg[0][i]!= '\n'; i++)
         {
             c = ENCmodpow(msg[0][i],e,n);
-            printf("%llu \n", c);
-            fputc(c, outfile);
-
+          //  printf("%llu \n", c);
+            fprintf(outfile, "%llu\n", c);
         }
     printf("\n");
     fclose(outfile);
@@ -246,7 +245,7 @@ void decrypt() {
                 	m1m2 += p;
        		h = (qInv * m1m2) % p;
         	m = m2 + h * q;
-        	fputc(m, outfile);
+        	fprintf(outfile, "%c", m);
             //printf("%c", m);
 	}
 	fclose(infile);
@@ -266,7 +265,7 @@ int main(int argc, char *argv[])
     int dec;
     char *s;
     //char* c[3] = {"˜"};
-    char* c[3] = {"1","2","3"};
+    char* c[11] = {"13", "14", "15", "\n"};
 
 //    if (argc != 4) {
 //        printf("Usage: RSA e/d infile outfile\n\te = encode\td = decode\n");
--- Software/rsa.c
+++ Software/rsa.c
@@ -159,17 +159,16 @@ void encrypt2(char* msg[]) {
 	int elements = sizeof(&msg);
 	unsigned long long int temp[elements];
 	printf("%c \n", msg[0]);
-	FILE *outp = fopen("ciphertext.txt", "w");
+	//FILE *outp = fopen("ciphertext.txt", "w");
     printf("ciphertext = ");
         for (i = 0; msg[0][i]!= '\n'; i++)
         {
             c = ENCmodpow(msg[0][i],e,n);
-            //printf("%llu \n", c);
-            fputc(c, outfile);
-
+          //  printf("%llu \n", c);
+            fprintf(outfile, "%llu\n", c);
         }
     printf("\n");
-    fclose(outp);
+    fclose(outfile);
 
 }
 
@@ -234,7 +233,7 @@ void decrypt() {
         fscanf(inp, "%d %d", &p, &q);
         fclose(inp);
 
-	while (c = fgetc(infile)) != EOF)
+	while (fscanf(infile, "%llu", &c) != EOF)
 	{
         	dP = d % (p - 1);
         	dQ = d % (q - 1);
@@ -246,8 +245,8 @@ void decrypt() {
                 	m1m2 += p;
        		h = (qInv * m1m2) % p;
         	m = m2 + h * q;
-        	fputc(m, outfile);
-            printf("%d", m);
+        	fprintf(outfile, "%c", m);
+            //printf("%c", m);
 	}
 	fclose(infile);
 	fclose(outfile);
@@ -266,7 +265,7 @@ int main(int argc, char *argv[])
     int dec;
     char *s;
     //char* c[3] = {"˜"};
-    char* c[3] = {"1","2","3"};
+    char* c[11] = {"13", "14", "15", "\n"};
 
 //    if (argc != 4) {
 //        printf("Usage: RSA e/d infile outfile\n\te = encode\td = decode\n");
